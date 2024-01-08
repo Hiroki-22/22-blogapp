ce1762142c37e24f20f3a7085d185fae597af7c1
Hiroki Ito <imainu.1222@gmail.com>
2024-01-08 14:02:46 +0900

    iiiiiii

--- babel.config.js
+++ babel.config.js
@@ -70,17 +70,3 @@ module.exports = function(api) {
     ].filter(Boolean)
   }
 }
-
-
-{
-  "presets": [
-    ["@babel/preset-env", {
-      "loose": false
-    }]
-  ],
-  "plugins": [
-    ["@babel/plugin-transform-class-properties", { "loose": true }],
-    ["@babel/plugin-transform-private-methods", { "loose": true }],
-    ["@babel/plugin-transform-private-property-in-object", { "loose": true }]
-  ]
-}
