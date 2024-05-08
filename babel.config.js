48da25fea73cc0040690fad7beb5939e9798b8a7
Hiroki Ito <imainu.1222@gmail.com>
2024-05-09 07:08:58 +0900

    action text をインストール

--- babel.config.js
+++ babel.config.js
@@ -42,7 +42,7 @@ module.exports = function(api) {
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
-        '@babel/plugin-transform-class-properties',
+        '@babel/plugin-proposal-class-properties',
         {
           loose: true
         }
@@ -54,13 +54,13 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-transform-private-methods',
+        '@babel/plugin-proposal-private-methods',
         {
           loose: true
         }
       ],
       [
-        '@babel/plugin-transform-private-property-in-object',
+        '@babel/plugin-proposal-private-property-in-object',
         {
           loose: true
         }
@@ -68,9 +68,7 @@ module.exports = function(api) {
       [
         '@babel/plugin-transform-runtime',
         {
-          helpers: false,
-          regenerator: true,
-          corejs: false
+          helpers: false
         }
       ],
       [
