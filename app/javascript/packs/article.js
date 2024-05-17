ee8db62bcd4eac8ef6cc588cf2756eab41059133
Hiroki Ito <imainu.1222@gmail.com>
2024-05-17 12:07:08 +0900

    エラー処理

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -36,6 +36,9 @@ document.addEventListener('turbolinks:load', () => {
         appendNewComment(comment)
       })
     })
+    .catch((error) => {
+      window.alert('失敗！')
+    })
 
   handleCommentForm()
 
